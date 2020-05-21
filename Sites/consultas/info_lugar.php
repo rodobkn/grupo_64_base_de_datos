302f2627f450e172e9665c5608e71cf439a8559a
Rodolfo <rrmendoza@uc.cl>
2020-05-21 15:45:08 -0400

    hola

--- Sites/consultas/info_lugar.php
+++ Sites/consultas/info_lugar.php
@@ -32,6 +32,23 @@
 	$result_3 -> execute();
     $caracteristicas_plazas = $result_3 -> fetchAll();
 
+
+    $query_4 = " SELECT ciudad, pais FROM Obras, Lugares_Obras, Lugares, Lugares_ciudades, Ciudades, Ciudades_contacto_paises WHERE
+    Obras.id_obra = Lugares_Obras.id_obra AND Lugares_Obras.id_lugar=Lugares.id_lugar AND Lugares.id_lugar=Lugares_Ciudades.id_lugar AND
+    Lugares_Ciudades.id_ciudad = Ciudades.id_ciudad AND Ciudades.id_ciudad=Ciudades_contacto_paises.id_ciudad AND Obras.id_obra=$id_lugar;";
+	$result_4 = $db_par -> prepare($query_4);
+	$result_4 -> execute();
+    $ciudad_y_pais_del_lugar = $result_4 -> fetchAll();
+
+    $query_5 = "SELECT Obras.id_obra, nombre_obra, fecha_1, fecha_2, nombre_artista FROM Lugares_Obras, Obras, Artistas_Obras, Artistas 
+    WHERE Lugares_Obras.id_obra = Obras.id_obra AND Obras.id_obra=Artistas_Obras.id_obra AND Artistas_Obras.id_artista=Artistas.id_artista
+    AND id_lugar=$id_lugar;";
+	$result_5 = $db_par -> prepare($query_5);
+	$result_5 -> execute();
+    $obras_y_artistas_del_lugar = $result_5 -> fetchAll();
+
+
+
     foreach ($caracteristicas_museo as $caracteristica) {
 
         $numero_tuplas_museos = $numero_tuplas_museos + 1;
@@ -50,6 +67,8 @@
 
   ?>
 
+<br>
+<br>
 
 <?php
 
@@ -66,6 +85,98 @@
 
 
 ?>
+
+<br>
+<br>
+
+<div class="container">
+
+  <div class="card">
+      <div class="card-header">
+          A continuación se mostrará la <b>ciudad</b> y <b>país</b> donde se encuentra <b> <?php echo "$nombre_lugar" ?> </b>:
+      </div>
+
+      <div class="card-body">
+
+
+      <table class="table">
+          <thead class="thead-dark">
+          <tr>
+              <th scope="col">Ciudad</th>
+              <th scope="col">País</th>
+
+          </tr>
+          </thead>
+          <tbody>
+              <!-- <th scope="row">1</th> -->
+
+              <?php
+              foreach ($ciudad_y_pais_del_lugar as $informacion) {
+                echo "<tr> <td>$informacion[0]</td> <td>$informacion[1]</td> </tr>";
+                }
+              ?>
+
+
+          </tbody>
+      </table>
+
+
+      </div>
+  </div>
+</div>
+
+
+<br>
+<br>
+
+<div class="container">
+
+  <div class="card">
+      <div class="card-header">
+          A continuación se mostrarán todas las <b>obras</b>, con sus respectivos <b>artistas</b>, que están a su disposición en el lugar <b> <?php echo "$nombre_lugar" ?> </b>:
+      </div>
+
+      <div class="card-body">
+
+
+      <table class="table">
+          <thead class="thead-dark">
+          <tr>
+              <th scope="col">Nombre de la obra</th>
+              <th scope="col">Fecha de inicio de la obra</th>
+              <th scope="col">Fecha de culminación de la obra</th>
+              <th scope="col">Artista creador</th>
+
+          </tr>
+          </thead>
+          <tbody>
+              <!-- <th scope="row">1</th> -->
+
+              <?php
+              foreach ($obras_y_artistas_del_lugar as $informacion) {
+
+                $variable_id_obra = $informacion[0];
+                $variable_nombre_obra = $informacion[1];
+                $variable_fecha_inicio = $informacion[2];
+                $variable_fecha_culminacion = $informacion[3];
+                $variable_nombre_artista = $informacion[4];
+
+
+                echo "<tr> <td> <a href='info_obra.php?nombre_obra=$variable_nombre_obra&id=$variable_id_obra'> $variable_nombre_obra </a> </td> 
+                <td>$variable_fecha_inicio</td> <td>$variable_fecha_culminacion</td> 
+                <td> <a href='info_artista.php?nombre=$variable_nombre_artista'> $variable_nombre_artista </a> </td> </tr>";
+
+                }
+              ?>
+
+
+          </tbody>
+      </table>
+
+
+      </div>
+  </div>
+</div>
 
 
 
