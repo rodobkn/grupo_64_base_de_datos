8a56973dd454e0a3ca7624de29980db2ad6e27a9
Rodolfo <rrmendoza@uc.cl>
2020-05-30 18:37:28 -0400

    hola

--- Sites/tickets/asiento_repetido.php
+++ Sites/tickets/asiento_repetido.php
@@ -1,1 +1,9 @@
-<h1>REPITIO ASIENTO, USTED NO APRENDE!</h1>
+
+<div class="container">
+    <div class="card">
+    <div class="card-body">
+        Usted indicó un <b>número de asiento que ya estaba ocupado</b>. Si quiere comprar un ticket asegurese de
+        elegir un asiento disponible, gracias.
+    </div>
+    </div>
+</div>
