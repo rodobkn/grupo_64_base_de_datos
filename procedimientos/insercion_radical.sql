2f8dbca7dbe0955034be2babe45708b2d746a785
rodobkn <rrmendoza@uc.cl>
2020-05-31 19:15:18 -0400

    hola

--- procedimientos/insercion_radical.sql
+++
@@ -1,1 +0,0 @@
-CREATE OR REPLACE FUNCTION insercion_radical (numero integer) RETURNS void AS $$ DECLARE temp varchar; BEGIN FOR i IN 1..numero LOOP temp := to_char(i,'99999999'); insert into personas values (temp,temp,temp); END LOOP; END $$ language plpgsql
--- procedimientos/insertar_persona.sql
+++
@@ -1,4 +0,0 @@
-CREATE OR REPLACE FUNCTION insertar_persona (rut varchar, nombre varchar, apellido varchar) 
-RETURNS void AS $$
- BEGIN insert into personas values (rut,nombre,apellido); 
-END $$ language plpgsql
