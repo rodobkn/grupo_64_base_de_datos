ae22384d8bc4b65704fffb8ea2801bef12a5d81f
Rodolfo <rrmendoza@uc.cl>
2020-05-31 06:15:49 -0400

    hola

--- Sites/tickets/asiento_disponible.php
+++ Sites/tickets/asiento_disponible.php
@@ -1,6 +1,38 @@
-<b> <?php echo "$id_usuario " ?></b>
-<b> <?php echo "$numero_de_asiento " ?></b>
-<b> <?php echo "$id_destino " ?></b>
-<b> <?php echo "$fecha_de_compra " ?></b>
-<b> <?php echo "$fecha_de_viaje " ?></b>
-<b> <?php echo "$id_del_ticket_nuevo " ?></b>
+
+<?php
+
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("../config/conexion.php");
+
+
+
+    $query = "INSERT INTO tickets VALUES($id_del_ticket_nuevo, $id_destino, $id_usuario, $numero_de_asiento,'$fecha_de_compra', '$fecha_de_viaje');";
+	$result = $db_impar -> prepare($query);
+	$result -> execute();
+
+  ?>
+
+<br>
+<br>
+
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            Compra de Ticket exitosa!
+        </div>
+    </div>
+</div>
+
+
+
+<?php include('../templates/footer.html'); ?>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom JavaScript for this theme -->
+<script src="js/scrolling-nav.js"></script>
